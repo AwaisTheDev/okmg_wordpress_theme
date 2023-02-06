28c374a42a2a26f01f15d1776b0c3c8e21ee1b5b
Muhammad Awais-1 <itsme84006@gmail.com>
2023-02-07 01:30:49 +0800

    deleted extra file

--- header.php
+++ header.php
@@ -11,8 +11,6 @@
     <link
         href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
         rel="stylesheet">
-    <script src="https://apis.google.com/js/api.js"></script>
-
 
     <?php wp_head();?>
 </head>
